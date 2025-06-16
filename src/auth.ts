b5b5770a17b1e524eb5d7ee9b227d57a00e3bae2
PizaaRiaaa <mm.rikkimae@gmail.com>
2025-06-16 23:15:53 +0800

    feat: added databased and drizzle

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,14 @@
 import NextAuth from 'next-auth';
 import Github from 'next-auth/providers/github';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+
+import { db } from '@/db/drizzle';
+import { accounts, users } from '@/db/schema';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+  }),
   providers: [Github],
 });
