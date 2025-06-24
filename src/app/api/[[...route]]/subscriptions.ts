9b71753212b0c4a584a98bf6606f472b8002ea01
PizaaRiaaa <mm.rikkimae@gmail.com>
2025-06-24 22:50:15 +0800

    bugfix: bypass eslint error

--- src/app/api/[[...route]]/subscriptions.ts
+++ src/app/api/[[...route]]/subscriptions.ts
@@ -79,7 +79,8 @@ const app = new Hono()
                 customerId: subscription.customer as string,
                 priceId: subscription.items.data[0].price.product as string,
                 currentPeriodEnd: new Date(
-                    (subscription.current_period_end as number) * 1000
+                    // @ts-ignore
+                    subscription.current_period_end * 1000
                 ),
                 createdAt: new Date(),
                 updatedAt: new Date(),
