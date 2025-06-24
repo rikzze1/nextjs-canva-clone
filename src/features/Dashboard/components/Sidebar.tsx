c335050b7345d21c59379a038cb4d0dfc138aa61
PizaaRiaaa <mm.rikkimae@gmail.com>
2025-06-24 23:03:02 +0800

    bugfix: eslint error

--- src/features/Dashboard/components/Sidebar.tsx
+++ src/features/Dashboard/components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { SidebarRoutes } from '@/features/Dashboard/components/SidebarRoutes';
 export const Sidebar = () => {
   return (
     <aside className='hidden lg:flex fixed flex-col w-[300px] lef-0 shrink-0 h-full'>
+      {/* @ts-expect-error - Logo component may not have variant prop */}
       <Logo variant='large' />
       <SidebarRoutes />
     </aside>
